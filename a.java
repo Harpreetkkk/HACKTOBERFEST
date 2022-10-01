d0c62e2c170a7d3541972ffd4790e97b0a7d16e2
amansvg <64675386+amansvg@users.noreply.github.com>
2022-10-01 09:39:46 +0530

    asd

---
+++ a.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+import java.awt.*;
+public class a {
+    public static void main(String[] args){
+        Date now=new Date();
+        now.getTime();
+        System.out.println(now);
+        Point nq=new Point(1,2);
+        System.out.println(nq);
+    }
+}
